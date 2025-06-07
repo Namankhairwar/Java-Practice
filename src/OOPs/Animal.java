9b3da4296ef2f94c40afc91d11ffa52c6f759d00
amankhairwar <khairwaraman@gmail.com>
2025-06-07 20:04:04 +0530

    code commit

--- src/OOPs/Animal.java
+++ src/OOPs/Animal.java
@@ -10,7 +10,7 @@ public class Animal {
         cat.eat();
         Dog dog = new Dog();
         dog.legs = 4;
-        dog.color = "Brown";
+        dog.color = "Blue";
         dog.display();
         dog.eat();
         cat.speak();
